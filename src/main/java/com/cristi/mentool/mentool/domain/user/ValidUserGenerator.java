b4c77c497e2196405c4e79228dc6ddebc7224a86
Cristi <cristi_zmf@yahoo.com>
2019-03-17 11:41:40 +0200

    cleaned the project

--- src/main/java/com/cristi/mentool/mentool/domain/user/ValidUserGenerator.java
+++
@@ -1,9 +0,0 @@
-package com.cristi.mentool.mentool.domain.user;
-
-public class ValidUserGenerator {
-
-    public static final User CRISTI = new User(
-            new EmailAddress("cristi@cristi.com"), "Cristi", "Cucumber warchief",
-            new PhoneNumber("1234567890")
-    );
-}
--- src/test/java/com/cristi/mentool/mentool/PxiogramApplicationTests.java
+++ src/test/java/com/cristi/mentool/mentool/PxiogramApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MentoolApplicationTests {
+public class PxiogramApplicationTests {
 
 	@Test
 	public void contextLoads() {
