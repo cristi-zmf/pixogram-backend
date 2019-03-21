0bd445889c12ef9f98f9ea3c0a8b386d7c02576b
cristi-zmf <cristi_zmf@yahoo.com>
2019-03-21 02:05:41 +0200

    upload endpoint

--- src/main/java/com/cristi/pixogram/exposition/ImagesResource.java
+++ src/main/java/com/cristi/pixogram/exposition/ImagesResource.java
@@ -2,27 +2,32 @@ package com.cristi.pixogram.exposition;
 
 import com.cristi.pixogram.domain.EmailAddress;
 import com.cristi.pixogram.domain.UniqueId;
+import com.cristi.pixogram.domain.userimage.ImageService;
+import com.cristi.pixogram.domain.userimage.UploadImageCommand;
 import com.cristi.pixogram.domain.userimage.UserImage;
 import com.cristi.pixogram.domain.userimage.UserImages;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Set;
 
 import static java.util.stream.Collectors.toSet;
 
 @MentoolRequestMapping
-public class GetImagesResource {
+public class ImagesResource {
     private final UserImages userImages;
+    private final ImageService imageService;
 
-    public GetImagesResource(UserImages userImages) {
+    public ImagesResource(UserImages userImages, ImageService imageService) {
         this.userImages = userImages;
+        this.imageService = imageService;
     }
 
     @GetMapping("/{username}/images/ids")
@@ -49,5 +54,14 @@ public class GetImagesResource {
         return ResponseEntity.ok()
                 .contentType(MediaType.IMAGE_JPEG)
                 .body(new InputStreamResource(new FileInputStream(imageFile)));
+    }
+
+    @PostMapping(value = "/full-images", consumes="multipart/form-data")
+    public ResponseEntity<UniqueId> getFullImage(
+            @RequestPart("multipartFile") MultipartFile multipartFile, @RequestPart("username") String username
+    ) throws IOException {
+        UploadImageCommand command = new UploadImageCommand(new EmailAddress(username), multipartFile);
+        UniqueId imageId = imageService.uploadImage(command);
+        return ResponseEntity.ok(imageId);
     }
 }
